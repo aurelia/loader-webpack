8f12aed620c9107d8c342734b46b4eea71cf3ec2
Jørn Andre Sundt <jorn.andre.sundt@jasmin.no>
2022-09-06 18:06:12 +0200

    Remove /*# sourceURL= */ from css-loader content

--- src/aurelia-loader-webpack.ts
+++ src/aurelia-loader-webpack.ts
@@ -10,6 +10,27 @@ declare global {
   }
 }
 
+type CssLoaderModuleItem = [
+  srcModule: string,
+  cssContent: string,
+  mediaAtRule?: string,
+  sourceMap?: {
+    version: number;
+    names: string[];
+    sourceRoot: string;
+    sources: string[];
+    sourcesContent: string[];
+    mappings: string;
+  },
+  supportsAtRule?: string,
+  layerAtRule?: string,
+];
+
+type CssLoaderModule = CssLoaderModuleItem[] & {
+  toString(): string;
+  i: Function;
+}
+
 export type LoaderPlugin = { fetch: (address: string) => Promise<TemplateRegistryEntry> | TemplateRegistryEntry };
 
 /**
@@ -236,11 +257,37 @@ export class WebpackLoader extends Loader {
     const result = await this.loadModule(url, false);
     // css-loader could use esModule:true
     const defaultExport = result && result.__esModule ? result.default : result;
-    if (defaultExport instanceof Array && defaultExport[0] instanceof Array && defaultExport.hasOwnProperty('toString')) {
+    if (this.isCssLoaderModule(defaultExport)) {
       // we're dealing with a file loaded using the css-loader:
-      return defaultExport.toString();
+      return this.getCssText(defaultExport);
     }
     return typeof result === "string" ? result : defaultExport;
+  }
+
+  /**
+   * Check if a loaded module is a css-loader module
+   * @param o The loaded module
+   * @returns `true` when {@link o} is a {@link CssLoaderModule}; otherwise false
+   */
+  private isCssLoaderModule(o: any): o is CssLoaderModule {
+    return o instanceof Array && o[0] instanceof Array && o.hasOwnProperty('toString');
+  }
+
+  /**
+   * Get CSS text from loaded css-loader module
+   * @param cssLoaderModule The {@link CssLoaderModule}
+   * @returns The css content with potential source map references
+   */
+  private getCssText(cssLoaderModule: CssLoaderModule): string {
+    let result = cssLoaderModule.toString();
+
+    // If some css-loader modules include source maps,
+    // ensure /*# sourceURL=[...] */ is removed to avoid chrome devtools problems
+    if (cssLoaderModule.some(m => m[3])) {
+      result = result.replace(/^\/\*# sourceURL=.* \*\/\s*\n/gm, "");
+    }
+
+    return result;
   }
 
   /**
@@ -333,31 +380,31 @@ export interface WebpackHotModule {
 
   /**
    * Return one of idle, check, watch, watch-delay, prepare, ready, dispose, apply, abort or fail.
-   * 
+   *
    * - `idle`
    * The HMR is waiting for your call the check(). When you call it the status will change to check.
-   * 
+   *
    * - `check`
    * The HMR is checking for updates. If it doesn’t find updates it will change back to idle. If updates were found it will go through the steps prepare, dispose and apply. Than back to idle.
-   * 
+   *
    * - `watch`
    * The HMR is in watch mode and will automatically be notified about changes. After the first change it will change to watch-delay and wait for a specified time to start the update process. Any change will reset the timeout, to accumulate more changes. When the update process is started it will go through the steps prepare, dispose and apply. Than back to watch or watch-delay if changes were detected while updating.
-   * 
+   *
    * - `prepare`
    * The HMR is prepare stuff for the update. This may means that it’s downloading something.
-   * 
+   *
    * - `ready`
    * An update is available and prepared. Call apply() to continue.
-   * 
+   *
    * - `dispose`
    * The HMR is calling the dispose handlers of modules that will be replaced.
-   * 
+   *
    * - `apply`
    * The HMR is calling the accept handlers of the parents of replaced modules, than it requires the self accepted modules.
-   * 
+   *
    * - `abort`
    * A update cannot apply, but the system is still in a (old) consistent state.
-   * 
+   *
    * - `fail`
    * A update has thrown an exception in the middle of the process, and the system is (maybe) in a inconsistent state. The system should be restarted.
    */
